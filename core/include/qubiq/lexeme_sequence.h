f74d3276136084f1ef2aadd04b094c6767657a29
Anton Soldatov <igelhaus@gmail.com>
2014-08-11 21:22:43 +0400

    LexemeSequence: preliminary documentation created

--- core/include/qubiq/lexeme_sequence.h
+++ core/include/qubiq/lexeme_sequence.h
@@ -14,14 +14,15 @@ class QUBIQSHARED_EXPORT LexemeSequence: public QObject {
     Q_OBJECT
 
 public:
+    //! LexemeSequenceState: enumeration for indicating states of the sequence.
     enum LexemeSequenceState {
-        STATE_OK             = 0,
-        STATE_EMPTY          = 1,
-        STATE_UNIGRAM        = 2,
-        STATE_BAD_BOUNDARY   = 3,
-        STATE_BAD_OFFSET     = 4,
-        STATE_BAD_OFFSET_N   = 5,
-        STATE_HAS_BOUNDARIES = 6
+        STATE_OK             = 0, //!< Valid sequence.
+        STATE_EMPTY          = 1, //!< Invalid: Sequence is empty.
+        STATE_UNIGRAM        = 2, //!< Invalid: Contains less than 2 lexemes.
+        STATE_BAD_BOUNDARY   = 3, //!< Invalid: Incorrectly split into subsequences.
+        STATE_BAD_OFFSET     = 4, //!< Invalid: Sequence offset does not fit the text length.
+        STATE_BAD_OFFSET_N   = 5, //!< Invalid: Offset + length do not fit the text length.
+        STATE_HAS_BOUNDARIES = 6  //!< Invalid: Sequence includes boundary lexemes.
     };
 
     LexemeSequence();
@@ -30,53 +31,108 @@ public:
     ~LexemeSequence();
 
     LexemeSequence &operator =(const LexemeSequence &other);
+    QString image(const Text *text) const;
 
+    //! Returns state of the sequence.
+    //! \sa LexemeSequenceState
+    //! \sa isValid
     inline LexemeSequenceState state() const { return _state; }
 
-    inline bool isValid()   const { return _state == LexemeSequence::STATE_OK; }
-    inline int  length()    const { return _lexemes->length(); }
-    inline int  n1()        const { return _n1; }
-    inline int  frequency() const { return _offsets->length(); }
+    //! Returns \c true if the object represents a valid lexeme sequence, and \c false otherwise.
+    //! \sa LexemeSequenceState
+    //! \sa state
+    inline bool isValid() const { return _state == LexemeSequence::STATE_OK; }
 
+    //! Returns the number of lexemes that compose the sequence.
+    //! \sa n1
+    inline int length() const { return _lexemes->length(); }
+
+    //! Returns the number of lexemes that compose the first subsequence.
+    //! \sa length
+    inline int n1() const { return _n1; }
+
+    //! Returns the number of ocurrences of the sequence in the source text.
+    inline int frequency() const { return _offsets->length(); }
+
+    //! Returns a pointer to the vector mapping lexemes of the sequence to
+    //! their offsets in the source text's vector of all lexemes.
+    //! \sa Text::lexemes
     inline const QVector<int>* lexemes() const { return _lexemes; }
-    inline const QVector<int>* offsets() const { return _offsets; }
-    inline const QByteArray*   key()     const { return _key; }
 
-    inline double mi()    const { return _mi; }
-    inline double llr()   const { return _llr; }
+    //! Returns a pointer to the vector of all offsets of the first lexeme
+    //! in the source text.
+    //! \sa Text::offsets
+    inline const QVector<int>* offsets() const { return _offsets; }
+
+    //! Returns a key of the sequence, a special internal value for implementing hashes and sets of sequences.
+    inline const QByteArray* key() const { return _key; }
+
+    //! Returns mutual information between the two subsequences composing the sequence.
+    //! \sa llr
+    //! \sa score
+    inline double mi() const { return _mi; }
+
+    //! Returns log-likelihood ratio between the two subsequences composing the sequence.
+    //! \sa mi
+    //! \sa score
+    inline double llr() const { return _llr; }
+
+    //! Returns overall score of the sequence.
+    //! \sa mi
+    //! \sa llr
     inline double score() const { return _score; }
 
-    inline int leftExpansionDistance()  const { return _led; }
+    //! Returns current left expansion distance of the sequence.
+    //! \sa rightExpansionDistance
+    //! \sa incLeftExpansionDistance
+    //! \sa incRightExpansionDistance
+    inline int leftExpansionDistance() const { return _led; }
+
+    //! Returns current right expansion distance of the sequence.
+    //! \sa leftExpansionDistance
+    //! \sa incLeftExpansionDistance
+    //! \sa incRightExpansionDistance
     inline int rightExpansionDistance() const { return _red; }
 
+    //! Increments current left expansion distance of the sequence by the value of \c n (1 by default).
+    //! \sa leftExpansionDistance
+    //! \sa rightExpansionDistance
+    //! \sa incRightExpansionDistance
     inline void  incLeftExpansionDistance(int n = 1) { _led += n; }
-    inline void incRightExpansionDistance(int n = 1) { _red += n; }
 
-    QString image(const Text *text) const;
+    //! Increments current right expansion distance of the sequence by the value of \c n (1 by default).
+    //! \sa leftExpansionDistance
+    //! \sa rightExpansionDistance
+    //! \sa incLeftExpansionDistance
+    inline void incRightExpansionDistance(int n = 1) { _red += n; }
 
 private:
     LexemeSequenceState _state;
 
-    int _n1;
+    int _n1; //!< Length of the first subsequence
 
-    /* Metrics of a lexeme sequence:
-     * 1) Mutual information
-     * 2) Log-likelihood ratio
-     * 3) Overall score
-     */
-    double _mi;
-    double _llr;
-    double _score;
+    double _mi;    //!< Mutual information
+    double _llr;   //!< Log-likelihood ratio
+    double _score; //!< Overall score
 
     // Expansion history of the sequence
-    int _led; /* Left Expansion Distance */
-    int _red; /* Right Expansion Disatnce */
+    int _led; //!< Left Expansion Distance
+    int _red; //!< Right Expansion Disatnce
 
-    QVector<int> *_lexemes;
-    QVector<int> *_offsets;
-    QByteArray   *_key; /* Sequence key for hashing */
+    QVector<int> *_lexemes; //!< Offsets of the lexeme in the lexeme vector of the text
+    QVector<int> *_offsets; //!< Offsets of the first lexeme of the sequence
+    QByteArray   *_key;     //!< Sequence key for hashing
 
-    /* aux function for counting log-likelihood ratio: */
+    /**
+     * \brief Auxiliary function for counting log-likelihood ratio.
+     *
+     * This is just a logged probability mass function for binomial distribution
+     * without binomial coefficient in the formula.
+     *
+     * \param[in] p Assumed probability of the success.
+     * \param[in] k Number of successes.
+     * \param[in] n Number of trials.
+     */
     inline double ll(double p, int k, int n) const {
         return k * log(p) + (n - k) * log(1 - p);
     }
@@ -93,6 +149,12 @@ private:
     bool is_sequence        (const Text *text, int text_offset, int sequence_offset, int n) const;
 };
 
+/**
+ * \brief Comparison operator for the two sequences.
+ * \param[in] s1 First sequence.
+ * \param[in] s2 Second sequence.
+ * \returns \c true if both sequences contain the same lexemes in the same order, \c false otherwise.
+ */
 inline bool operator ==(const LexemeSequence &s1, const LexemeSequence &s2)
 {
     const QVector<int> *l1 = s1.lexemes();
@@ -106,6 +168,12 @@ inline bool operator ==(const LexemeSequence &s1, const LexemeSequence &s2)
     return true;
 }
 
+/**
+ * \brief Overloaded qHash function for calculating hash value of the sequence
+ * \param sequence Lexeme sequence.
+ * \param seed     Seed to initialize hashing procedure.
+ * \returns Hash value of the sequence.
+ */
 inline uint qHash(const LexemeSequence &sequence, uint seed) {
     return qHash(*(sequence.key()), seed);
 }
