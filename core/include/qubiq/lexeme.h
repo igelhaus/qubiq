58ff7ca45c7a263d265e696be0350d95fe78e37a
Anton Soldatov <igelhaus@gmail.com>
2015-06-24 09:20:31 +0300

    Lexeme: introducing operator=, copy constructor and constructing a lexeme solely by its name

--- core/include/qubiq/lexeme.h
+++ core/include/qubiq/lexeme.h
@@ -6,8 +6,7 @@
 
 // 2DO: describe arbitrary lexeme features as a free-form QHash?
 
-class QUBIQSHARED_EXPORT Lexeme: public QObject {
-    Q_OBJECT
+class QUBIQSHARED_EXPORT Lexeme {
 
 private:
 
@@ -21,16 +20,28 @@ private:
     QVector<int>     *_offsets;
     QHash<int, int>  *_idx_offsets; // Ensure that offsets are unique
 
+    void _initialize(const QString &name, bool is_boundary);
+    void _destroy();
+    void _assign(const Lexeme &other);
+
 public:
-    Lexeme(const QString &lexeme, bool is_boundary);
+    Lexeme(const QString &name);
+    Lexeme(const Lexeme &other);
     ~Lexeme();
 
+    Lexeme(const QString &lexeme, bool is_boundary); // FIXME: Remove this one
+
+    Lexeme &operator =(const Lexeme &other);
+
     inline QString lexeme()     const { return _lexeme; }
+    inline QString name()       const { return _lexeme; }
     inline bool    isBoundary() const { return _is_boundary; }
     inline bool    isVirtual()  const { return _forms->length() == 0; }
 
     inline const QVector<QString>* forms() const { return _forms; }
     inline const QVector<int>*   offsets() const { return _offsets; }
+
+    inline void setIsBoundary(bool is_boundary) { _is_boundary = is_boundary; }
 
     bool addForm(const QString &form, int offset, bool overwrite = false);
 };
--- core/src/lexeme.cpp
+++ core/src/lexeme.cpp
@@ -1,19 +1,24 @@
 #include <qubiq/lexeme.h>
 
+Lexeme::Lexeme(const QString &name)
+{
+    _initialize(name, false);
+}
+
+//! Copy constructor. Constructs a Lexeme object from the other Lexeme object.
+Lexeme::Lexeme(const Lexeme &other)
+{
+    _assign(other);
+}
+
 Lexeme::Lexeme(const QString &lexeme, bool is_boundary)
 {
-    _lexeme      = lexeme;
-    _is_boundary = is_boundary;
-    _forms       = new QVector<QString>();
-    _offsets     = new QVector<int>();
-    _idx_offsets = new QHash<int, int>;
+    _initialize(lexeme, is_boundary);
 }
 
 Lexeme::~Lexeme()
 {
-    delete _forms;
-    delete _offsets;
-    delete _idx_offsets;
+    _destroy();
 }
 
 bool Lexeme::addForm(const QString &form, int offset, bool overwrite /* = false */)
@@ -28,4 +33,46 @@ bool Lexeme::addForm(const QString &form, int offset, bool overwrite /* = false 
         _idx_offsets->insert(offset, _forms->length() - 1);
     }
     return true;
+}
+
+/**
+ * \brief Assignment operator.
+ * \param[in] other Another lexeme sequence to assign to the current object.
+ * \returns Reference to the original object after assignment.
+ */
+Lexeme &Lexeme::operator =(const Lexeme &other)
+{
+    if (this != &other) {
+        _destroy();
+        _assign(other);
+    }
+    return *this;
+}
+
+//! \internal Initializes class members.
+void Lexeme::_initialize(const QString &name, bool is_boundary)
+{
+    _lexeme      = name;
+    _is_boundary = is_boundary;
+    _forms       = new QVector<QString>();
+    _offsets     = new QVector<int>();
+    _idx_offsets = new QHash<int, int>();
+}
+
+//! \internal Assigns \c other members to \c this members.
+void Lexeme::_assign(const Lexeme &other)
+{
+    _lexeme      = other._lexeme;
+    _is_boundary = other._is_boundary;
+    _forms       = new QVector<QString>(*(other._forms));
+    _offsets     = new QVector<int>(*(other._offsets));
+    _idx_offsets = new QHash<int, int>(*(other._idx_offsets));
+}
+
+//! \internal Frees memory occupied by class members.
+void Lexeme::_destroy()
+{
+    delete _forms;
+    delete _offsets;
+    delete _idx_offsets;
 }
